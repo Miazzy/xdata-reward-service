0cb963e4a72513a5c4ccee25c1e91c02fd07c262
yunwisdom <qq695926920@163.com>
2020-12-09 19:17:54 +0800

    init

--- src/components/reward/rewardapply.vue
+++ src/components/reward/rewardapply.vue
@@ -1579,13 +1579,13 @@ export default {
                   const response = await query.queryRoleGroupList('COMMON_REWARD_HR_ADMIN' , this.item.hr_id);
 
                   //获取到印章管理员组信息
-                  let user_group_ids = response && response.length > 0 ? response[0].userlist : '';
-                  let user_group_names = response && response.length > 0 ? response[0].enuserlist : '';
+                  let user_group_ids = response && response.length > 0 ? response[0].userlist + `,${userinfo.username}` : '';
+                  let user_group_names = response && response.length > 0 ? response[0].enuserlist + `,${userinfo.username}` : '';
 
                   //如果未获取用户名称，则直接设置用印人为分组成员
                   if(tools.isNull(user_group_ids)){
-                    user_group_ids = this.item.hr_id;
-                    user_group_names = this.item.hr_name;
+                    user_group_ids = this.item.hr_id + `,${userinfo.username}`;
+                    user_group_names = this.item.hr_name + `,${userinfo.username}`;
                   }
                   // 返回预览URL
                   const receiveURL = encodeURIComponent(`${window.requestAPIConfig.rewarddomain}/#/reward/rewardview?id=${id}&pid=&tname=bs_reward_apply&panename=myrewardlist&typename=hr_admin_ids&bpm_status=4&proponents=${user_group_ids}&role=hr`);
@@ -1938,12 +1938,12 @@ export default {
                   //查询直接所在工作组，注意此处是奖罚人力经理管理员
                   const response = await query.queryRoleGroupList('COMMON_REWARD_HR_ADMIN' , this.item.hr_id);
                   //获取到印章管理员组信息
-                  let user_group_ids = response && response.length > 0 ? response[0].userlist : '';
-                  let user_group_names = response && response.length > 0 ? response[0].enuserlist : '';
+                  let user_group_ids = response && response.length > 0 ? response[0].userlist + `,${userinfo.username}` : '';
+                  let user_group_names = response && response.length > 0 ? response[0].enuserlist + `,${userinfo.username}` : '';
                   //如果未获取用户名称，则直接设置用印人为分组成员
                   if(tools.isNull(user_group_ids)){
-                    user_group_ids = this.item.hr_id;
-                    user_group_names = this.item.hr_name;
+                    user_group_ids = this.item.hr_id + `,${userinfo.username}`;
+                    user_group_names = this.item.hr_name + `,${userinfo.username}`;
                   }
                   // 返回预览URL
                   const receiveURL = encodeURIComponent(`${window.requestAPIConfig.rewarddomain}/#/app/reward?id=${id}&statustype=office&type=${type}&role=hr`);
